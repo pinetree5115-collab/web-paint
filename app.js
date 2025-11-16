17f182320da4b9ed594d4f2129d48e6f09b95c84
Yerin_Kim <pinetree5115@gmail.com>
2025-11-16 23:02:16 +0900

    feat: 마우스 이벤트를 이용한 기본 그리기 기능 구현

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d")
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
+let isPainting = false;
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moverTo(event.offsetX, event.offsetY);
+}
+function onMouseDown() {
+  isPainting = true;
+}
+function onMouseUp() {
+  isPainting = false;
+}
 
-
-ctx.fillRect(200. 200, 50, 200);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
