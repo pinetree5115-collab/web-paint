5887a1c4b1eba3e45c2a1c85c1b808394f0a1bbe
Yerin_Kim <pinetree5115@gmail.com>
2025-12-01 12:57:31 +0900

    fix: 캔버스 영역 이탈 시 드로잉이 멈추지 않는 버그 수정

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
 
 const canvas = document.querySelector("canvas");
+
 const ctx = canvas.getContext("2d")
+
 canvas.width = 800;
+
 canvas.height = 800;
+
 ctx.lineWidth = 2;
+
 let isPainting = false;
+
 function onMove(event) {
+
   if (isPainting) {
+
     ctx.lineTo(event.offsetX, event.offsetY);
+
     ctx.stroke();
+
     return;
+
   }
+
   ctx.moveTo(event.offsetX, event.offsetY);
-}
-function onMouseDown() {
-  isPainting = true;
-}
-function onMouseUp() {
-  isPainting = false;
+
 }
 
+function startPainting() {
+
+  isPainting = true;
+
+}
+
+function cancelPainting() {
+
+  isPainting = false;
+
+}
+
+
 canvas.addEventListener("mousemove", onMove);
-canvas.addEventListener("mousedown", onMouseDown);
-canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting)
