ae40c969ffa09d6a920e965883564bbfa895096c
Yerin_Kim <pinetree5115@gmail.com>
2025-12-02 23:59:05 +0900

    feat: 선 색상 변경 기능 추가

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+const color = document.getElementById("color")
 const lineWidth = document.getElementById("line-width")
 const canvas = document.querySelector("canvas");
 
@@ -30,9 +32,15 @@ function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
 
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting)
 
 lineWidth.addEventListener("change", onLineWidthChange)
+color.addEventListener("change", onColorChange)
